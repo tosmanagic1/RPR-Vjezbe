f88256041116209d9f27e1ad7ed3c0a6b3db0b08
tosmanagic1 <tosmanagic1@etf.unsa.ba>
2023-11-07 00:34:24 +0100

    vjezba3, zadatak3

--- src/main/java/ba/unsa/etf/rpr/Izuzetak.java
+++ src/main/java/ba/unsa/etf/rpr/Izuzetak.java
@@ -1,7 +1,8 @@
 package ba.unsa.etf.rpr;
 
 public class Izuzetak extends RuntimeException {
-    public Izuzetak(String message) {
+        public Izuzetak(String message) {
         super(message);
     }
+
 }
