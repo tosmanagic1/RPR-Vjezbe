ac5c0abf2d5e54551227482c3af50c5705e20e39
tosmanagic1 <tosmanagic1@etf.unsa.ba>
2023-11-07 12:28:34 +0100

    Postavljen maven.

---
+++ lv4-z1/src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    /*@Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }*/
+}
