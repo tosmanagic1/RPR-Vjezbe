2b9cd43862bc713935989b9659735216f5a48cbe
tosmanagic1 <tosmanagic1@etf.unsa.ba>
2023-11-14 13:20:01 +0100

    Napravljene klase LicneInformacije, InformacijeOStudentu, InformacijeONastavku

---
+++ lv5-z1/src/main/java/ba/unsa/etf/rpr/InformacijeONastavku.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.rpr;
+
+public class InformacijeONastavku {
+    String ime;
+    String prezime;
+    String titula;
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getTitula() {
+        return titula;
+    }
+
+    public void setTitula(String titula) {
+        this.titula = titula;
+    }
+}
---
+++ lv5-z1/src/main/java/ba/unsa/etf/rpr/InformacijeOStudentu.java
@@ -0,0 +1,41 @@
+package ba.unsa.etf.rpr;
+
+public class InformacijeOStudentu {
+
+    String ime;
+    String prezime;
+    String godinaStudija;
+    String brojIndexa;
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getGodinaStudija() {
+        return godinaStudija;
+    }
+
+    public void setGodinaStudija(String godinaStudija) {
+        this.godinaStudija = godinaStudija;
+    }
+
+    public String getBrojIndexa() {
+        return brojIndexa;
+    }
+
+    public void setBrojIndexa(String brojIndexa) {
+        this.brojIndexa = brojIndexa;
+    }
+}
