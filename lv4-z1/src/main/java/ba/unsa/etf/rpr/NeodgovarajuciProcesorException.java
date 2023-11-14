e753137582d2e5d58858bb1f31b669ef0e0c0b6d
tosmanagic1 <tosmanagic1@etf.unsa.ba>
2023-11-14 12:14:34 +0100

    Moj prazni commit zbog kreiranja brancha

--- lv4-z1/src/main/java/ba/unsa/etf/rpr/NeodgovarajuciProcesorException.java
+++
@@ -1,7 +0,0 @@
-package ba.unsa.etf.rpr;
-
-public class NeodgovarajuciProcesorException extends RuntimeException {
-    public NeodgovarajuciProcesorException(String message) {
-        super(message);
-    }
-}
