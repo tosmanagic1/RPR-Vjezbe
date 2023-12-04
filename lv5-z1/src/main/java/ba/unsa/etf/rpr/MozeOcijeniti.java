3d1a6331f575c3c7f5f35aa775fb20e47249950f
tosmanagic1 <tosmanagic1@etf.unsa.ba>
2023-12-04 21:04:35 +0100

    Moj prazni commit zbog kreiranja brancha

--- lv5-z1/src/main/java/ba/unsa/etf/rpr/MozeOcijeniti.java
+++
@@ -1,6 +0,0 @@
-package ba.unsa.etf.rpr;
-
-public interface MozeOcijeniti {
-
-    Ocjena ocijeni (Integer x);
-}
--- lv5-z1/src/main/java/ba/unsa/etf/rpr/Predstavi.java
+++
@@ -1,6 +0,0 @@
-package ba.unsa.etf.rpr;
-
-public interface Predstavi {
-
-    String predstavi();
-}
